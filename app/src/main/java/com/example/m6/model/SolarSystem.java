8204e28c849496b2ee0332ce6e3c02963592f24d
schoi369 <schoi369@gatech.edu>
2019-02-28 19:18:03 -0500

    Update SolarSystem.java
    IE, CR, ER이 한 솔라 시스템에 동시에 여러 개 적용 될 수도 있기 때문에 이런 형태로 추가.

--- app/src/main/java/com/example/m6/model/SolarSystem.java
+++ app/src/main/java/com/example/m6/model/SolarSystem.java
@@ -9,6 +9,29 @@ public class SolarSystem {
     private TechLevel techLevelValue;
     private Resource resourceValue;
     private Planet planet;
+    
+    //솔라 시스템의 IE, CR, ER.
+    public boolean DROUGHT;
+    public boolean LOTSOFWATER;
+    public boolean DESERT;
+    public boolean COLD;
+    public boolean RICHFAUNA;
+    public boolean LIFELESS;
+    public boolean CROPFAIL;
+    public boolean RICHSOIL;
+    public boolean POORSOIL;
+    public boolean WAR;
+    public boolean MINERALRICH;
+    public boolean MINERALPOOR;
+    public boolean BOREDOM;
+    public boolean ARTISTIC;
+    public boolean WARLIKE;
+    public boolean PLAGUE;
+    public boolean LOTSOFHERBS;
+    public boolean LACKOFWORKERS;
+    public boolean BOREDOM;
+    public boolean WEIRDMUSHROOMS;
+    
     SolarSystem(String name, int x, int y, int techLevel, int resource) {
         Planet planet = new Planet(name, x, y);
         this.name = name;
@@ -18,6 +41,27 @@ public class SolarSystem {
         this.resource = resource;
         resourceValue = Resource.values()[resource];
         techLevelValue = TechLevel.values()[techLevel];
+        
+        //솔라 시스템에 IE, CR, ER 랜덤하게 생성
+        DROUGHT = (Math.random() < 0.5);
+        LOTSOFWATER = (Math.random() < 0.5);
+        DESERT = (Math.random() < 0.5);
+        COLD = (Math.random() < 0.5);
+        RICHFAUNA = (Math.random() < 0.5);
+        LIFELESS = (Math.random() < 0.5);
+        CROPFAIL = (Math.random() < 0.5);
+        RICHSOIL = (Math.random() < 0.5);
+        POORSOIL = (Math.random() < 0.5);
+        WAR = (Math.random() < 0.5);
+        MINERALRICH = (Math.random() < 0.5);
+        MINERALPOOR = (Math.random() < 0.5);
+        BOREDOM = (Math.random() < 0.5);
+        ARTISTIC = (Math.random() < 0.5);
+        WARLIKE = (Math.random() < 0.5);
+        PLAGUE = (Math.random() < 0.5);
+        LOTSOFHERBS = (Math.random() < 0.5);
+        LACKOFWORKERS = (Math.random() < 0.5);
+        WEIRDMUSHROOMS = (Math.random() < 0.5);
     }
     public String getName() {
         return name;
